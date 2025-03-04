405de660e15196cfab7925b76c72355e2e1a91fe
asus2 <izmailoff.ehduard@yandex.ru>
2025-03-04 19:31:53 +0300

    ++

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,21 +1,21 @@
 package org.skypro.skyshop.product;
 
 public class Product {
-    private final String name; // Название продукта
-    private final int price;   // Стоимость продукта
+    private final String name;
+    private final int price;
 
-    // Конструктор для инициализации продукта
+
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
-    // Геттер для названия продукта
+
     public String getName() {
         return name;
     }
 
-    // Геттер для стоимости продукта
+
     public int getPrice() {
         return price;
     }
